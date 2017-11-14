aae9b94c5cac510dc61a04c8ba75b8a4c4fb8ce4
hanmiton <hanmilton_12@outlook.com>
2017-11-14 16:39:12 -0500

    modificando ruta de static

--- server/main.js
+++ server/main.js
@@ -12,7 +12,7 @@ var messages = [{
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + 'public/index.html');
+  res.sendFile(../public/index.html);
 });
 
 app.get('/hello', function(req, res) {
